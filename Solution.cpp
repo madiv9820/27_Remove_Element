1f039ec36439089a18bc89dd4bc206ad764075b8
madiv9820 <themathur901@gmail.com>
2024-10-04 19:36:34 +0530

    C++ Solution

--- Solution.cpp
+++ Solution.cpp
@@ -5,35 +5,25 @@ using namespace std;
 class Solution {
     public:
         int removeElement(vector<int>& nums, int val) {
-            // Create a temporary vector to store elements from 'nums'
-            // that are not equal to 'val'
-            vector<int> temp_Vector;
-            
-            // Iterate over all elements in 'nums'
-            // Add elements that are not equal to 'val' to 'temp_Vector'
-            for (const int& x : nums)
-                if (x != val) 
-                    temp_Vector.emplace_back(x);
-            
-            // Copy the elements from 'temp_Vector' back into 'nums'
-            // This modifies 'nums' in place for the required part of the array
-            for (int index = 0; index < temp_Vector.size(); ++index)
-                nums[index] = temp_Vector[index];
-            
-            // Return the new size of 'nums', which is the size of 'temp_Vector'
-            return temp_Vector.size();
+            // 'actual_Index' keeps track of the position where the next valid element (i.e., not equal to 'val') will be placed.
+            int actual_Index = 0;
+
+            // Iterate through all elements in 'nums'
+            for (int index = 0; index < nums.size(); ++index)
+                // If the current element is not equal to 'val', move it to 'actual_Index' position
+                if (nums[index] != val) 
+                    nums[actual_Index++] = nums[index]; // Increment 'actual_Index' after placing the element
+
+            // Return the new length of the array (number of elements not equal to 'val')
+            return actual_Index;
         }
 };
 
 // Time Complexity:
-// The first loop iterates through the 'nums' array, and for each element not equal to 'val', it adds it to 'temp_Vector', taking O(n) time.
-// The second loop copies elements from 'temp_Vector' back into 'nums', which also takes O(n) time.
-// Thus, the overall time complexity is O(n), where 'n' is the number of elements in 'nums'.
+// The loop runs through all elements of the input vector 'nums', making the time complexity O(n), where 'n' is the number of elements in 'nums'.
 
 // Space Complexity:
-// We are using an extra vector 'temp_Vector' to store the filtered elements, which takes O(n) additional space.
-// Therefore, the space complexity is O(n), where 'n' is the size of the input vector 'nums'.
-
+// The algorithm modifies the vector in place and uses only a few extra variables (actual_Index), making the space complexity O(1), i.e., constant space.
 
 int main() {
     vector<int> nums = {3,2,2,3}; int val = 3;
